a871fd37ec6352bcb4d2d4768927ba59c4311733
Christophe_M2i <cduthoit@live.fr>
2023-05-02 10:52:00 +0200

    ok

--- signup.php
+++ signup.php
@@ -47,28 +47,28 @@ include_once 'header.php';
               <div class="d-grid">
                 <button class="btn btn-primary btn-login text-uppercase fw-bold" type="submit">Créer le compte</button>
               </div>
-              <label><?php
-                      // Messages d'erreur
-                      if (isset($_GET["error"])) {
-                        if ($_GET["error"] == "emptyinput") {
-                          echo "<p>Remplir tous les champs&nbsp;!</p>";
-                        } else if ($_GET["error"] == "invaliduid") {
-                          echo "<p>Renseigner un nom valide&nbsp;!</p>";
-                        } else if ($_GET["error"] == "invalidemail") {
-                          echo "<p>Renseigner une adresse mail valide&nbsp;!</p>";
-                        } else if ($_GET["error"] == "passwordsdontmatch") {
-                          echo "<p>Saisir un mot de passe&nbsp;!</p>";
-                        } else if ($_GET["error"] == "stmtfailed") {
-                          echo "<p>Erreur, contacter l'administrateur du site&nbsp;!</p>";
-                        } else if ($_GET["error"] == "usernametaken") {
-                          echo "<p>Pseudo déjà utilisé, choisir un autre&nbsp;!</p>";
-                        } else if ($_GET["error"] == "none") {
-                          echo "<p>Vous êtes enregistré&nbsp;!</p>";
-                        }
-                      }
-                      ?></label>
+              <label>
+                <?php
+                // Messages d'erreur
+                if (isset($_GET["error"])) {
+                  if ($_GET["error"] == "emptyinput") {
+                    echo '<div class="alert alert-danger" role="alert">Remplir tous les champs&nbsp;!</div>';
+                  } else if ($_GET["error"] == "invaliduid") {
+                    echo '<div class="alert alert-danger" role="alert">Renseigner un nom valide&nbsp;!</div>';
+                  } else if ($_GET["error"] == "invalidemail") {
+                    echo '<div class="alert alert-danger" role="alert">Renseigner une adresse mail valide&nbsp;!</div>';
+                  } else if ($_GET["error"] == "passwordsdontmatch") {
+                    echo '<div class="alert alert-danger" role="alert">Saisir un mot de passe&nbsp;!</div>';
+                  } else if ($_GET["error"] == "stmtfailed") {
+                    echo '<div class="alert alert-danger" role="alert">Erreur, contacter l\'administrateur du site&nbsp;!</div>';
+                  } else if ($_GET["error"] == "usernametaken") {
+                    echo '<div class="alert alert-danger" role="alert">Pseudo déjà utilisé, choisir un autre&nbsp;!</div>';
+                  } else if ($_GET["error"] == "none") {
+                    echo '<div class="alert alert-success" role="alert">Vous êtes enregistré&nbsp;!</div>';
+                  }
+                }
+                ?></label>
             </form>
-
           </div>
         </div>
       </div>
