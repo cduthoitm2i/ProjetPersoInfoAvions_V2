9061102c254cf5508c656f2e14123582c4dc6966
Christophe_M2i <cduthoit@live.fr>
2023-05-02 08:34:12 +0200

    ok

--- contact.php
+++ contact.php
@@ -1,50 +1,79 @@
 <?php
 include_once 'header_contact.php';
-require_once("./inc/init.inc.php");
-
-
-// Message de fin
-$reponses = [];
-// Vérification du bon remplissage de tous les champs
-if (isset($filter_input['email'], $filter_input['sujet'], $filter_input['nom'], $filter_input['prenom'], $filter_input['message'])) {
-    // Validation adresse mail
-    if (!filter_var($filter_input['email'], FILTER_VALIDATE_EMAIL)) {
-        $reponses[] = 'Adresse mail invalide!';
+//require_once('./inc/init.inc.php');
+?>
+<?php
+// Variable pour message de fin
+$responses = [];
+// Vérification si on récupère bien les informations des champs
+if (isset($_POST['email'], $_POST['sujet'], $_POST['nom'], $_POST['message'])) {
+    // Validation de l'adresse mail
+    if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+        $responses[] = 'Email is not valid!';
     }
-    // Vérification que tous les champs ne sont pas vide
-    if (empty($filter_input['email']) || empty($filter_input['sujet']) || empty($filter_input['nom']) || empty($filter_input['prenom']) || empty($filter_input['message'])) {
-        $reponses[] = 'Compléter tous les champs!';
+    // Vérification si les champs sont remplis
+    if (empty($_POST['email']) || empty($_POST['sujet']) || empty($_POST['nom']) || empty($_POST['message'])) {
+        $responses[] = 'Please complete all fields!';
     }
-    // Si aucune erreur avant, on continue
-    if (!$reponses) {
-        // A qui envoyer le mail
-        $to = 'cduthoit@live.fr';
-        // 
+    // si pas d'erreurs, on peut envoyer le mail
+    if (!$responses) {
+        // A qui envoyé un mail
+        $to      = 'cduthoit@live.fr';
+        // Send mail from which email address?
         $from = 'cduthoit@gmail.com';
-        // Sujet du message
-        $sujet = $filter_input['sujet'];
-        // Mail message
-        $message = $filter_input['message'];
-        // Mail entête (avant on paramètre pour les caractères spéciaux soient corrects dans l'entête du mail de réception en définissant le codage utf-8)
-        // Test effectué avec des emojis, résultat ok
+        // Sujet du mail
+        $sujet = $_POST['sujet'];
+        // Message du mail
+        $message = $_POST['message'];
+        // Entête de l'email
         $headers = 'MIME-Version: 1.0' . "\n";
         $headers .= 'Content-type: text/plain; charset=utf-8' . "\n";
         $headers .= 'Content-Transfer-Encoding: 8bit' . "\n";
-        $headers .= 'From: ' . $from . "\r\n" . 'Reply-To: ' . $filter_input['email'] . "\r\n" . 'X-Mailer: PHP/' . phpversion();
-
+        $headers = 'From: ' . $from . "\r\n" . 'Reply-To: ' . $_POST['email'] . "\r\n" . 'X-Mailer: PHP/' . phpversion();
         // Essai d'envoi du mail
         if (mail($to, $sujet, $message, $headers)) {
-            // Envoi réussi
-            $reponses[] = 'Message bien envoyé!';
+            // Mail envoyé
+            //$responses[] = 'Message envoyé&nbsp;!';
+            $responses[] = '<div class="alert alert-success" role="alert">Message bien envoyé&nbsp;!</div>';
         } else {
-            // Echec
-            $reponses[] = 'Message pas envoyé! Merci de vérifier la configuration de votre serveur mail!';
+            // Mail pas envoyé
+            //$responses[] = 'Message pas envoyé&nbsp;! Vérifier vos paramètres de compte mail&nbsp;!';
+            $responses[] = '<div class="alert alert-danger" role="alert">Message pas envoyé&nbsp;! Merci de vérifier la configuration de votre serveur mail&nbsp;!</div>';
         }
     }
 }
 ?>
-<section class="signup-form">
-    <div class="container signup-form-form">
+<section>
+    <!--<div class="container signup-form-form">
+        <div class="row">
+            <div class="col-sm-9 col-md-7 col-lg-7 mx-auto">
+                <div class="card border-0 shadow rounded-3 my-5">
+                    <div class="card-body p-4 p-sm-5">
+                        <h5 class="card-title text-center mb-5 fw-light fs-4">Formulaire de contact</h5>
+                        <form class="contact" method="post" action="">
+                            <div class="fields">
+                                <label for="email">
+                                    <i class="fas fa-envelope"></i>
+                                    <input id="email" type="email" name="email" placeholder="Votre email" required>
+                                </label>
+                                <label for="name">
+                                    <i class="fas fa-user"></i>
+                                    <input type="text" name="name" placeholder="Votre nom" required>
+                                    <label>
+                                        <input type="text" name="subject" placeholder="Votre message" required>
+                                        <textarea name="msg" placeholder="Message" required></textarea>
+                            </div>
+                            <input type="submit">
+                            <?php if ($responses) : ?>
+                                <p class="responses"><?php echo implode('<br>', $responses); ?></p>
+                            <?php endif; ?>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>-->
+    <div class="container">
         <div class="row">
             <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
                 <div class="well well-sm card border-0 shadow rounded-3 my-5 p-4 p-sm-5">
@@ -54,53 +83,51 @@ if (isset($filter_input['email'], $filter_input['sujet'], $filter_input['nom'], 
                             <!-- champs pour votre nom-->
                             <div class="form-group">
                                 <div class="form-floating mb-3">
-                                    <input class="form-control" id="nom" type="text" placeholder="Nom" aria-label="Nom" value="<?php if (isset($_POST['nom'])) echo htmlspecialchars($_POST['nom']); ?>" name="nom" required>
+                                    <input class="form-control" id="nom" type="text" placeholder="Nom" aria-label="nom" value="<?php if (isset($_POST['nom'])) echo htmlspecialchars($_POST['nom']); ?>" name="nom">
                                     <label for="nom" class="col-sm-2 col-form-label">Nom</label>
                                 </div>
                             </div>
-
                             <!-- champs pour votre prénom-->
                             <div class="form-group">
-
                                 <div class="form-floating mb-3">
-                                    <input class="form-control" id="prenom" type="text" placeholder="Prénom" aria-label="Prenom" name="prenom" value="<?php if (isset($_POST['prenom'])) echo htmlspecialchars($_POST['prenom']); ?>" required>
+                                    <input class="form-control" id="prenom" type="text" placeholder="Prénom" aria-label="prenom" name="prenom" value="<?php if (isset($_POST['prenom'])) echo htmlspecialchars($_POST['prenom']); ?>" >
                                     <label for="prenom" class="col-sm-2 col-form-label">Prénom</label>
                                 </div>
                             </div>
                             <!-- champs pour le sujet du message-->
                             <div class="form-group">
                                 <div class="form-floating mb-3">
-                                    <input class="form-control" id="sujet" type="text" placeholder="Sujet" aria-label="Sujet" name="sujet" value="<?php if (isset($_POST['sujet'])) echo htmlspecialchars($_POST['sujet']); ?>" required>
+                                    <input class="form-control" id="sujet" type="text" placeholder="Sujet" aria-label="sujet" name="sujet" value="<?php if (isset($_POST['sujet'])) echo htmlspecialchars($_POST['sujet']); ?>" >
                                     <label for="sujet" class="col-sm-2 col-form-label">Sujet du message</label>
                                 </div>
                             </div>
                             <!-- champs pour l'email -->
                             <div class="form-group">
                                 <div class="form-floating mb-3">
-                                    <input type="email" class="form-control" id="email" placeholder="E-mail" name="email" value="<?php if (isset($_POST['email'])) echo htmlspecialchars($_POST['email']); ?>" required>
+                                    <input type="email" class="form-control" id="email" placeholder="E-mail" aria-label="email" name="email" value="<?php if (isset($_POST['email'])) echo htmlspecialchars($_POST['email']); ?>" >
                                     <label class="col-sm-2 col-form-label" for="email">E-mail</label>
-                                    <!--<span class="error"><?php echo $emailErr; ?></span>-->
                                 </div>
                             </div>
                             <!-- champs pour le message -->
                             <div class="form-group mb-3">
                                 <div class="col-md-12">
-                                    <textarea class="form-control" id="message" name="message" placeholder="Merci de saisir votre message..." rows="5"><?php if (isset($_POST['commentaire'])) echo htmlspecialchars($_POST['commentaire']); ?></textarea>
+                                    <textarea class="form-control" id="message" name="message" placeholder="Merci de saisir votre message..." rows="5"><?php if (isset($_POST['message'])) echo htmlspecialchars($_POST['message']); ?></textarea>
                                 </div>
                             </div>
                             <!-- bouton d'envoi -->
                             <div class="form-group">
                                 <div class="form-group form-floating mb-3">
                                     <div class="d-grid col-md-12 text-right">
-                                        <button class="btn btn-primary btn-login text-uppercase fw-bold" type="submit">Envoyer</button>
+                                        <button class="btn btn-primary btn-login text-uppercase fw-bold" type="submit" value="Envoyer">Envoyer</button>
                                     </div>
                                 </div>
                             </div>
                         </fieldset>
-                        <?php if ($reponses) : ?>
-                            <p class="reponses"><?php echo implode('<br>', $reponses); ?></p>
+                        <?php if ($responses) : ?>
+                            <?php echo implode($responses); ?>
                         <?php endif; ?>
                     </form>
+
                 </div>
             </div>
         </div>
