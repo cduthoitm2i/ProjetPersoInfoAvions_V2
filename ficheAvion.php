31ad89b32b0f352c0cd1a47a981a81becf8b46c4
Christophe_M2i <cduthoit@live.fr>
2023-05-11 11:56:15 +0200

    ok

--- ficheAvion.php
+++ ficheAvion.php
@@ -5,6 +5,7 @@ include './includes/setvariable.php';
 ?>
 <section>
     <div class="container">
+        <h1>Fiche avion</h1>
         <div class="row row-cols-1 row-cols-md-2 g-4">
             <div class="col">
                 <div class="card mb-5">
--- ficheCompagnie.php
+++ ficheCompagnie.php
@@ -3,47 +3,33 @@ include_once 'header.php';
 ?>
 <section>
     <div class="container">
-        <h1>Fiche avion</h1>
+        <h1>Fiche compagnie</h1>
         <div class="row row-cols-1 row-cols-md-2 g-4">
             <div class="col">
                 <div class="card mb-5">
                     <div class="card-body">
-                        <h5 class="card-title">Chercher un avion ou une compagnie</h5>
-                        <form class="d-flex">
-                            <input class="form-control me-2" type="search" aria-label="Search">
-                            <button class="btn btn-outline-success" type="submit">Chercher</button>
-                        </form>
-                        <p class="p-2 card-text text-center">Saisir une immatriculation, un&nbsp;numéro de&nbsp;série ou&nbsp;le&nbsp;nom d'une&nbsp;compagnie aérienne</p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col">
-                <div class="card mb-5">
-                    <div class="card-body">
-                        <h5 class="card-title">Liste de production</h5>
+                        <h5 class="card-title">NOM COMPAGNIE&nbsp;: <!--<?php echo "$nomCompagnie" ?>--></h5>
                         <table class="table table-sm table-borderless">
-
                             <tbody>
                                 <tr>
-                                    <td>Numéro de série</td>
-                                    <td></td>
+                                    <td class="small">Pays&nbsp;:</td>
+                                    <td class="small">drapeau image</td>
                                 </tr>
                                 <tr>
-                                    <td>Type</td>
-                                    <td></td>
+                                    <td class="small">Date&nbsp;:</td>
+                                    <td class="small"></td>
                                 </tr>
                                 <tr>
-                                    <td>Date premier vol</td>
-                                    <td></td>
+                                    <td class="small">Code&nbsp;:</td>
+                                    <td class="small"></td>
                                 </tr>
                                 <tr>
-                                    <td>Immatriculation d'essai</td>
-                                    <td></td>
+                                    <td class="small">Callsign&nbsp;:</td>
+                                    <td class="small"></td>
                                 </tr>
                                 <tr>
-                                    <td>Age de l'avion</td>
-                                    <td></td>
+                                    <td class="small">siteweb&nbsp;:</td>
+                                    <td class="small">www.xxx.com</td>
                                 </tr>
                             </tbody>
                         </table>
