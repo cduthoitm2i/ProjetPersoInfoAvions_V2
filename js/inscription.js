69a8f6c24503cb100a1d5019882f40c1d2dde2e4
Christophe_M2i <cduthoit@live.fr>
2023-07-06 11:04:44 +0200

    ok

--- js/inscription.js
+++ js/inscription.js
@@ -34,7 +34,7 @@ function valider() {
     } else {
       document.getElementById("formulaireAuthentification").submit();
       ("<br/><div class='d-grid alert alert-danger' role='alert'>Compte crée</div>");
-      alert("Votre compte est crée ! Vous pouvez vous connecter.");
+      //alert("Votre compte est crée ! Vous pouvez vous connecter.");
       //window.alert("<div class='modal' tabindex='-1'><div class='modal-dialog'><div class='modal-content'><div class='modal-header'><h5 class='modal-title'>Modal title</h5><button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button></div><div class='modal-body'><p>Modal body text goes here.</p></div><div class='modal-footer'><button type='button' class='btn btn-secondary' data-bs-dismiss='modal'>Close</button><button type='button' class='btn btn-primary'>Save changes</button></div></div></div></div>");
 
       window.location = "login.php"; // Redirection vers la page de login
